2a3509baff2fde3419c92eda8d9cc6904941b0f5
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-08-14 03:06:35 +0200

    Fixed toggle_bit function

--- TrafficLightSystem/MCAL/utilities/bit_manipulation.h
+++ TrafficLightSystem/MCAL/utilities/bit_manipulation.h
@@ -11,7 +11,7 @@
 
 #define SET_BIT(registerName, bitNumber) (registerName |= (1 << bitNumber))
 #define CLR_BIT(registerName, bitNumber) (registerName &= ~(1 << bitNumber))
-#define TOGGLE_BIT(registerName, bitNumber) (registerName ^= ~(1 << bitNumber))
+#define TOGGLE_BIT(registerName, bitNumber) (registerName ^= (1 << bitNumber))
 #define GET_BIT(registerName, bitNumber) ((registerName & (1 << bitNumber)) >> bitNumber)
 
 
