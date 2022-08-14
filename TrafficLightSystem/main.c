223d20034d11321b33006c31df19d4e1b979293b
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-08-14 03:06:27 +0200

    Edited main

--- TrafficLightSystem/main.c
+++ TrafficLightSystem/main.c
@@ -4,34 +4,20 @@
  * Created: 22/08/11 03:00:41 PM
  * Author : VGA
  */ 
-#define F_CPU 8000000UL
-#include <avr/io.h>
- 
-//$include <avr/delay.h>
-#include "MCAL/timer/timer.h"
+#define F_CPU 1000000UL
 
-// toggle PORTB status
-void toggle() {
+#include "APP/app.h"
 
-	PORTB = ~PORTB;
 
-}
 
 int main(void)
 {
-		// set all pins of PORTB as output
-		DDRB = 0xff;
-		PORTB = 0xff;
-	// toggle PORTB every 500ms (using 16Mhz clock)
-	timer1(TIMER1_PRESCALER_1024, 39063U, toggle);
-	
-		// enable
 		sei();
-    /* Replace with your application code */
+	Ped_Button_Init();
+	TF_Init(0);
     while (1) 
     {
-		//_delay_ms(10);
-		//PORTB = ~PORTB;
+
     }
 }
 
